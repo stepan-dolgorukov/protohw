6460ccf1b438d5580cc493c3d62ce8d8c6faf958
Stёpa Dolgorukov <63650851+stepan-dolgorukov@users.noreply.github.com>
2023-05-31 20:19:22 +0500

    Запуск сервера с аргументами

--- sntp-server/server.py
+++ sntp-server/server.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
 
 import socket
+import argparse
+import sys
 from reply import timestamp_from_request, make_reply_packet
 
-def run_server(port):
+def run_server(port, delay):
     UDP_IP = ""
     UDP_PORT = port
 
@@ -11,10 +13,33 @@ def run_server(port):
                          socket.SOCK_DGRAM)
     sock.bind((UDP_IP, UDP_PORT))
 
+    print(f"Сервер успешно запущен. Порт: {args.port}, смещение-обманка: {args.delay} сек.")
+
     while True:
         data, sender = sock.recvfrom(128)
-        responce = make_reply_packet(data, 10)
+        responce = make_reply_packet(data, delay)
         sock.sendto(responce, sender)
 
 if __name__ == '__main__':
-    run_server(123)
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--port", "-p", type=int, help="Порт, который будет слушать сервер")
+    parser.add_argument("--delay", "-d", type=int, help="Смещение-обманка")
+
+    args = parser.parse_args()
+
+    if args.port is None:
+        print("Не указан порт.", file=sys.stderr)
+        exit(1)
+
+    if args.delay is None:
+        print("Не указано смещение.", file=sys.stderr)
+        exit(1)
+
+    try:
+        run_server(args.port, args.delay)
+    except PermissionError:
+        print(f"Недостаточно прав для запуска сервера на {args.port} порту.", file=sys.stderr)
+    except OverflowError as exc:
+        print(f"Недопустимое значение порта.", file=sys.stderr)
+    except KeyboardInterrupt:
+        print("Завершение работы.")
