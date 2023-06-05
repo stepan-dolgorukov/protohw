00109bec014093b92111506a0369346f048e731e
Stёpa Dolgorukov <63650851+stepan-dolgorukov@users.noreply.github.com>
2023-06-05 14:39:38 +0500

    Множество только из файлов, содержащих изображения

--- http-api-6/directory_regular_files.py
+++ http-api-6/directory_regular_files.py
@@ -1,10 +1,17 @@
 from os import listdir
 from pathlib import Path
+import magic
 
 def directory_regular_files(directory: Path):
     regular_files = set()
+    types_images = {"png", "jpeg"}
 
     for file in listdir(directory):
-        regular_files.add(file)
+        type_file = magic.from_file(directory / file).lower()
+
+        for type_img in types_images:
+            if type_file.startswith(type_img):
+                regular_files.add(file)
+                break
 
     return regular_files
