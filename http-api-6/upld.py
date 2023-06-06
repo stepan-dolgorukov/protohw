c7573051c9060adae884f564248f4c47d70c7f04
Stёpa Dolgorukov <63650851+stepan-dolgorukov@users.noreply.github.com>
2023-06-06 11:15:40 +0500

    Вывод сообщений об ошибках в stderr

--- http-api-6/upld.py
+++ http-api-6/upld.py
@@ -7,6 +7,7 @@ from argparse import ArgumentParser
 from directory_image_files import directory_image_files
 from access_token import access_token
 from pathlib import Path
+import sys
 
 if __name__ == '__main__':
     parser = ArgumentParser()
@@ -20,11 +21,11 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
     if args.aid is None:
-        print("Не задан идентификатор альбома.")
+        print("Не задан идентификатор альбома.", file=sys.stderr)
         exit(1)
 
     if args.dir is None:
-        print("Не задан каталог с файлами на загрузку.")
+        print("Не задан каталог с файлами на загрузку.", file=sys.stderr)
         exit(1)
 
     token = None
@@ -35,25 +36,25 @@ if __name__ == '__main__':
     try:
         token = access_token()
     except EnvironmentError as env_err:
-        print(env_err)
+        print(env_err, file=sys.stderr)
         exit(1)
 
     try:
         api = vk.API(access_token=token, v="5.131")
     except Exception as err:
-        print(err)
+        print(err, file=sys.stderr)
         exit(1)
 
     try:
         server = api.photos.getUploadServer(album_id=args.aid)
     except Exception as err:
-        print(err)
+        print(err, file=sys.stderr)
         exit(1)
 
     try:
         server_url = server["upload_url"]
     except Exception as err:
-        print(err)
+        print(err, file=sys.stderr)
         exit(1)
 
     for number_file, file in enumerate(
@@ -74,6 +75,6 @@ if __name__ == '__main__':
                 photos_list=json_responce["photos_list"],
                 hash=json_responce["hash"])
         except Exception as err:
-            print(f"Не удалось загрузить файл {file}")
-            print(err)
+            print(f"Не удалось загрузить файл {file}", file=sys.stderr)
+            print(err, file=sys.stderr)
             continue
